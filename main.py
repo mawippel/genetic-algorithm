329bbc204b31ee68de6d9f34c2a15f32cb1f5595
mawippel <mawippel2@hotmail.com>
2019-04-24 21:45:02 -0300

    feat: generates x and y arrays

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import random
 
 POPULATION_SIZE = 20
-CROMOSSOMES_SIZE = 20
 CITIES_SIZE = 20
 population = []
+x = []
+y = []
+tour = []
 
 def generateFirstPopulation():
   # For each position, generates a new possible path
-  for j in range(1, POPULATION_SIZE + 1):
+  for _ in range(1, POPULATION_SIZE + 1):
     generatePossiblePath()
 
 def generatePossiblePath():
   path = []
-  for x in range(1, CITIES_SIZE + 1):
-    randomNum = random.randint(1, 21) # generates a new number between 1 - 20
+  for _ in range(1, CITIES_SIZE + 1):
+    randomNum = random.randint(1, 20) # generates a new number between 1 - 20
     while(numberExistsInPath(path, randomNum)): # while the generated number exists in the list, generates a new one
       randomNum = random.randint(1, 20)
     path.append(randomNum)
@@ -25,6 +27,22 @@ def numberExistsInPath(path, number):
       return True
   return False
 
-if __name__ == "__main__":
+def generateXandY():
+  for _ in range(CITIES_SIZE):
+    randomNumber = random.random()
+    randomNumber = round(randomNumber,2)
+    x.append(randomNumber)
+
+    randomNumber = random.random()
+    randomNumber = round(randomNumber,2)
+    y.append(randomNumber)
+
+def main():
   generateFirstPopulation()
+  generateXandY()
+  print(x)
+  print(y)
   print(population)
+
+if __name__ == "__main__":
+  main()
