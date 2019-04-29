69cb3e23be52b02df3133576ab5f701a6a78abe8
mawippel <mawippel2@hotmail.com>
2019-04-29 20:04:01 -0300

    feat: generating children

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ CITIES_SIZE = 20
 population = []
 x = []
 y = []
+parentsOne = None
+parentsTwo = None
 dCidade = [[0 for x in range(POPULATION_SIZE)] for y in range(POPULATION_SIZE)]
 distances = [0 for x in range(POPULATION_SIZE)]
 
@@ -88,6 +90,7 @@ def calculateDistances():
     dict_dist = { i : distances[i] for i in range(0, len(distances) ) }
     distances = dict_dist
     sorted_x = sorted(distances.items(), key=lambda kv: kv[1])
+
     return sorted_x
 
 # Generate the identity matrix (dCidade)
@@ -105,16 +108,75 @@ def rouletteFunction(sorted_x):
     for j in range(len(arr)):
         for _ in range(10 - j):
             rouletteArr.append(arr[j])
-    parents1 = createParents(rouletteArr)
-    parents2 = createParents(rouletteArr)
-    print(parents1)
-    print(parents2)
+    global parentsOne
+    global parentsTwo
+    parentsOne = createParents(rouletteArr)
+    parentsTwo = createParents(rouletteArr)
+    print(parentsOne)
+    print(parentsTwo)
 
 def createParents(rouletteArr):
     parentArr = []
     for _ in range(5):
         parentArr.append(rouletteArr[random.randint(0, 54)])
     return parentArr
+
+def hasDuplicity(auxArray, usedIndexes):
+    for i in range(len(auxArray)):
+        for j in range(i, len(auxArray)):
+            if i != j and auxArray[i] == auxArray[j]:
+                if i in usedIndexes:
+                    return j
+                else:
+                    return i
+    return False
+
+def doCycle(sorted_x):
+    global population
+    children = []
+
+    # percorrer ate 5
+    for i in range(5):
+        parentOneAux = parentsOne[i]
+        parentTwoAux = parentsTwo[i]
+        usedIndexes = []
+    
+        randomIndexInsideCromossomus = random.randint(0, POPULATION_SIZE - 1)
+
+        usedIndexes.append(randomIndexInsideCromossomus)
+
+        childOne = copy.deepcopy(population[parentOneAux])
+        childTwo = copy.deepcopy(population[parentTwoAux])
+
+        valAuxOne = childOne[randomIndexInsideCromossomus]
+        valAuxTwo = childTwo[randomIndexInsideCromossomus]
+
+        childOne[randomIndexInsideCromossomus] = valAuxTwo
+        childTwo[randomIndexInsideCromossomus] = valAuxOne
+
+        while(hasDuplicity(childOne, usedIndexes) != False):
+            newIndex = hasDuplicity(childOne, usedIndexes)
+            usedIndexes.append(newIndex)
+
+            valAuxOne = childOne[newIndex]
+            valAuxTwo = childTwo[newIndex]
+
+            childOne[newIndex] = valAuxTwo
+            childTwo[newIndex] = valAuxOne
+
+        # apos gerar os filhos, ordena a populacao, e adiciona os filhos
+        # updates the population array
+
+        children.append(childOne)
+        children.append(childTwo)
+
+    print(children)
+
+    for i in range(10):
+        population[i] = copy.deepcopy(population[sorted_x[i][0]])
+    
+    
+    
 
 def main():
     # runs only once
@@ -124,7 +186,8 @@ def main():
     # runs in a loop 0 - 9999
     sorted_x = fitnessFunction()
     rouletteFunction(sorted_x)
-# TODO crossover
+    doCycle(sorted_x)
+
 # TODO mutacao
 
 
